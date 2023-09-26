c653cb4fdfa7337fe596f25a751d0010e07a2c4e
c0b22037 <c0b220378d@edu.teu.ac.jp>
2023-09-26 15:35:31 +0900

    koukaton3

--- pygame_image.py
+++ pygame_image.py
@@ -8,13 +8,20 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230926/fig/pg_bg.jpg")
     bg_img1 = pg.image.load("ex01-20230926/fig/3.png")
-    bg_img1 = pg.transform.flip(bg_img, True, False)
+    bg_img1 = pg.transform.flip(bg_img1, True, False)
+    bg_img2 = pg.transform.rotozoom(bg_img1, 10, 1.0)
+    imglist = [bg_img1, bg_img2]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        num = tmr%2
+        if num == 0:
+            screen.blit(imglist[num], [300, 200])
+        else:
+            screen.blit(imglist[num], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
